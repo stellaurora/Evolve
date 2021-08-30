def11fd5fce66a4db595ab38899fe107a1a272e7
jean1398reborn <jean1398reborn@gmail.com>
2021-08-30 13:16:53 +1000

    Update clamping so it doesnt go to infinity whoops

--- modules/entities.js
+++ modules/entities.js
@@ -247,7 +247,7 @@ var fabricatedKnowledge = {
 
         // find a new tree with berries, and the entity has also eaten one berry today  so add the amount of food its worth ^w^
         entity.treeTarget = {}
-        entity.treesEaten = clamp(entity.treesEaten + simulationFactors.foodValue, 0, entity.foodStorageCap);
+        entity.treesEaten =  clamp(entity.treesEaten + simulationFactors.foodValue, 0, entity.foodStorageCap);
 
       }
     }
